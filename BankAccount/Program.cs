726ad23048ce2fc072aeff0b2bc9636376b8f131
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-02-23 18:07:19 +0900

    실습 중간저장합니다.

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,4 +1,67 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine();
+BankAccount account = new BankAccount("123-456-789", "홍길동");
+account.ShowInfo();
+
+account.Deposit(50000);
+account.Deposit(30000);
+account.Withdraw(20000);
+account.Withdraw(100000);
+account.Deposit(-1000);
+
+account.ShowInfo();
+class BankAccount
+{
+    //필드 / 속성
+    public string AccountNumber { get; private set; }
+    public string OwnerName { get; private set; }
+    public decimal Balance { get; private set; }
+
+
+    //생성자
+
+    public BankAccount(string name, string acount)
+    {
+        OwnerName = name;
+        AccountNumber = acount;
+        Balance = 0;
+    }
+      
+    //메서드
+
+    //입금
+    public void Deposit(int amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("입금 금액은 0보다 커야합니다.");
+        }
+        else
+        {
+            Balance += amount;
+            Console.WriteLine($"{amount}원 입금 완료. 잔액: {Balance}");
+        }
+    }
+
+    //출금
+    public void Withdraw(int amount)
+    {
+        if (amount> Balance)
+        {
+            Console.WriteLine("잔액이 부족합니다.");
+        }
+        else if (amount>0)
+        {
+            Balance -= amount;
+            Console.WriteLine($"{amount}원 출금 완료. 잔액: {Balance}");
+        }
+    }
+    //출금정보
+    public void ShowInfo()
+    {
+        Console.WriteLine($" [계좌 정보] {AccountNumber} ({OwnerName}) - 잔액: {Balance}원");
+    }
+
+}
--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,4 +1,157 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine();
+
+//1.
+/*ClassName.Membername();
+class ClassName
+{
+    public static void Membername()
+    {
+        Console.WriteLine("클래스의 멤버가 호출되어 실행됩니다.");
+    }
+} 
+
+//2.
+string square = Square.GetName();
+Console.WriteLine(square);
+class Square
+{
+    public static string GetName()
+    {
+        return "정사각형";
+    }
+}
+
+
+//3.
+MyfirstClass.StaticMethod();
+
+MyfirstClass my = new MyfirstClass();
+my.InstaceMethod();
+
+class MyfirstClass
+{
+    public static void StaticMethod()
+    {
+        Console.WriteLine("[1] 정적 메서드");
+    }
+
+    public void InstaceMethod()
+    {
+        Console.WriteLine("[2] 인스턴스 메서드");
+    }
+}
+
+
+//4.
+Person person1 = new Person();
+person1.Name = "홍길동";
+person1.Age = 25;
+person1.Introduce();
+
+Person person2 = new Person();
+person2.Name = "김철수";
+person2.Age = 30;
+person2.Introduce();
+class Person
+{
+    public string Name;
+    public int Age;
+
+    public void Introduce()
+    {
+        Console.WriteLine($"안녕하세요, {Name}입니다. {Age}살입니다.");
+    }
+}
+
+
+//5.
+Point point1 = new Point();
+point1.X = 10;
+point1.Y = 20;
+
+Point point2 = point1;
+point2.X = 100;
+
+Console.WriteLine($"point1.X = {point1.X}");
+Console.WriteLine($"point2.X = {point2.X}");
+class Point
+{
+    public int X; 
+    public int Y; 
+}
+
+
+//6.
+PointStruct ps1 = new PointStruct();
+ps1.X = 10;
+PointStruct ps2 = ps1;
+ps2.X = 100;
+Console.WriteLine($"구조체: ps1.X = {ps1.X}, ps2.X = {ps2.X}");
+
+PointClass pc1 = new PointClass();
+pc1.X = 10;
+PointClass pc2 = pc1;
+pc2.X = 100;
+Console.WriteLine($"클래스: pcc1.X = {pc1.X}, pc2.X = {pc2.X}");
+struct PointStruct
+{
+    public int X;
+    public int Y;
+}
+
+class PointClass
+{
+    public int X;
+    public int Y;
+}
+
+
+//7.
+Calulator calc = new Calulator();
+Console.WriteLine($"3 + 5 = {calc.Add(3, 5)}");
+Console.WriteLine($"3 * 5 = {calc.Multiply(3, 5)}");
+class Calulator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Multiply(int a, int b) => a * b;
+}
+
+//8.
+Player player1 = new Player();
+player1.ShowInfo();
+
+Player player2 = new Player("용사", 150);
+player2.ShowInfo();
+class Player
+{
+    private string _name;
+    private int _health;
+
+    public Player()
+    {
+        _name = "Unknow";
+        _health = 100;
+    }
+    public Player(string name, int health)
+    {
+        _name = name;
+        _health = health;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"이름: {_name}, 체력: {_health}");
+    }
+}
+*/
+
+//9.
+
+//class
